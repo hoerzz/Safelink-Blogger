0f18b2049f12e567f598cc15eb4f47c517bd166a
hoerzz <rudinhoer160@gmail.com>
2022-05-26 02:48:02 +0700

    Update+++++++

--- safelink.js
+++ safelink.js
@@ -1283,10 +1283,10 @@ $(document).ready(function () {
     var request = !1;
     wcGetLink.off("click", function () {
         0 == request && (gotolinkcountdown(), (request = !0)), $("html, body").animate({ scrollTop: eval(gotolink.offset().top - 10) }, 500);
+    });
+    gotolink.on("click", function () {
+        var t = aesCrypto.decrypt(convertstr($.urlParam("o")), convertstr("root"));
+        window.location.href = t;
     }),
-        gotolink.on("click", function () {
-            var t = aesCrypto.decrypt(convertstr($.urlParam("o")), convertstr("root"));
-            window.location.href = t;
-        }),
         on(!1, function () { });
 });
